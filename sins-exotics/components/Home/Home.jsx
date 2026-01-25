ee91a6d829f13da60479509e17ec5d6770649a29
Daniel Anorue Jr. <danielanorue@gmail.com>
2026-01-25 12:57:01 -0600

    rules template will edit for a better design later

--- sins-exotics/components/Home/Home.jsx
+++ sins-exotics/components/Home/Home.jsx
@@ -7,31 +7,87 @@ function Home(){
 		<div>
 			<div className="intro">
 				<h1>Welcome to Sin's Exotics Car Rentals</h1>
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut mollitia quas dicta esse adipisci. Amet temporibus dolorem atque harum fugit labore, laborum, in, aspernatur placeat blanditiis fugiat quis obcaecati est.
-				Corporis, alias? Sequi odit, quidem praesentium eveniet quae fugiat aliquid suscipit unde quod repudiandae recusandae blanditiis ullam sapiente eligendi tenetur. Reprehenderit delectus fugit explicabo eum quasi quis officia? Quidem, qui?
-				Odit, architecto enim! Minus vero ab totam recusandae culpa adipisci perferendis ducimus, perspiciatis tempore quos praesentium dolores quis dolorum doloribus optio, amet sint sed iusto nulla. Dicta minus possimus molestias?
-				Quod alias, excepturi eum ipsam facilis adipisci velit nam et quasi officia recusandae enim ab, doloribus distinctio quaerat tenetur. Maxime quae accusantium facilis totam dolores culpa praesentium quis ex nam?
-				Sunt, nam! Quo commodi aut cupiditate, nisi ipsa tempore at hic ut qui optio ducimus tenetur. Similique, veritatis atque, explicabo reprehenderit eligendi ducimus molestiae debitis doloremque odio eum, quam nulla?
-				Veniam quo at ex sint est reiciendis aliquam assumenda quae, eveniet maxime quibusdam tempore aperiam dolore, fugiat ipsa molestiae debitis. Itaque est corrupti ipsa quam totam hic voluptates veritatis natus?
-				Officiis delectus sequi at praesentium, cum ullam explicabo nam ut doloribus adipisci corrupti impedit et corporis rem eius voluptate quasi quo voluptatibus aut illum cumque. Labore corrupti sint reiciendis praesentium!
-				Recusandae laudantium illum sint ipsum iste at, voluptate quis neque quo unde qui. Nulla debitis fugit voluptate at, nisi quam non facere eos itaque dolores. Placeat quisquam quidem excepturi voluptates!
-				Dolor assumenda quis aspernatur velit recusandae dolores esse magnam rerum unde consequuntur laborum architecto mollitia corrupti facilis nisi, accusamus tempore harum, temporibus accusantium nemo! Quos maiores aperiam culpa repellat molestias?
-				Sed quo recusandae nobis soluta enim rem aperiam obcaecati nihil delectus! Deserunt reprehenderit expedita enim ratione illo dicta beatae, a inventore. Ipsa officia cupiditate temporibus doloribus laudantium magni at quia.
-				Quisquam reiciendis aliquam possimus quod voluptate ratione obcaecati eveniet! Explicabo nobis maiores fugiat, enim eligendi illo omnis neque consequatur id laudantium quod assumenda ut est ipsum necessitatibus, provident odit culpa.
-				Nobis, reprehenderit asperiores facilis non sit molestias ducimus libero, cum provident veritatis praesentium mollitia hic, magnam illum reiciendis culpa expedita nemo quis iusto beatae aliquid. Ullam repellendus quo quisquam perspiciatis?
-				Magnam vero dicta quo iure nemo corrupti, assumenda vel et qui ducimus odio repellat quia voluptate sed aliquid veritatis. Repellendus accusantium blanditiis illo qui delectus sint temporibus soluta ipsum cumque.
-				Cumque iste repudiandae ea nobis. Quis repellat asperiores facilis ad provident perferendis architecto porro error fuga fugit mollitia dicta nisi iusto rem voluptatem cupiditate debitis, non doloribus libero. Dolorem, adipisci.
-				Nisi labore deleniti neque minus, eum voluptate? Dolor quaerat ex ullam obcaecati temporibus fugit? Atque, itaque voluptatibus? Corrupti illo cum tempore voluptates veniam rerum explicabo numquam, nam beatae nulla. Odio!
-				Voluptates voluptatem reprehenderit mollitia. Perferendis magnam quasi saepe possimus, pariatur placeat quisquam optio. Sint fuga doloremque, minima voluptates iste enim vitae sequi maxime est excepturi laborum odit mollitia ad illum?
-				Praesentium, impedit enim fugiat error facilis quis eligendi nobis quibusdam eveniet sapiente, nihil commodi earum maiores maxime. Sint pariatur nostrum ratione nihil corporis aliquid, omnis iste quisquam cupiditate maxime modi!
-				Corrupti tenetur officia at illo voluptate totam nostrum ipsam quo, blanditiis perferendis alias animi vero in quisquam laborum, commodi hic soluta ex iusto aspernatur. Modi similique id asperiores voluptatibus ea.
-				Est accusamus aliquid odit quasi libero nemo animi odio veniam cupiditate impedit, nam a, consectetur omnis eius error facilis! Dolorem id eum porro? Quisquam, temporibus suscipit. Cum distinctio neque iusto.
-				Fugit eligendi magnam dolore expedita, cumque dolorum modi hic minus perspiciatis in quia molestias voluptatum quas eos consectetur iste, esse illum est inventore. Quas fugit unde dignissimos sapiente deleniti aperiam?
-				Dolor rem at possimus harum? Dolores doloribus nobis, culpa assumenda quisquam ab, hic repudiandae tempore vel quis harum sint natus explicabo magnam, pariatur sit omnis. Cum modi molestiae vero eligendi!</p>
+				<p>
+					Experience elite service with a fleet that spans from high-end economy to world-class exotics.
+					Also featuring Luxury Chauffeur Services and seamless delivery, we provide the perfect ride for every occasion.
+				</p>
+
+				<div className="rules">
+					<h1>Rules</h1>
+					<div className="rule-cards">
+						<div className="card1">
+							<h3>Rule #1</h3>
+							<p className="rule-text">
+								<div className="rule-heading">
+									<strong>Follow all Traffic Rules and Regulations</strong>
+								</div>
+								<hr />
+								Renting the vehicle means any type of tickets or tolls
+								you get while the car is and was in your
+								possession will be at <span className="warning">your expense</span>.
+							</p>
+						</div>
+						<div className="card2">
+							<h3>Rule #2</h3>
+							<p className="rule-text">
+								<div className="rule-heading">
+									<strong>Keep the Car in the same condition
+								that you received it!</strong>
+								</div>
+								<hr />
+								Before you bring the car back, make
+								sure it's <strong>clean</strong> and <strong>neat</strong>.
+								Any <strong>damages</strong> to the vehicle inside and
+								out will be <span className="warning">your responsibility</span>.
+							</p>
+						</div>
+						<div className="card3">
+							<h3>Rule #3</h3>
+							<p className="rule-text">
+								<div className="rule-heading">
+									<strong>If I give you the keys, YOU are the person
+								responsible for the car.</strong>
+								</div>
+								<hr /> 
+								Do not give the car to anyone else because I will
+								not not hold anyone but <span className="warning">YOU </span>
+								accountable!!!!
+							</p>
+						</div>
+						<div className="card4">
+							<h3>Rule #4</h3>
+							<p className="rule-text">
+								<div className="rule-heading">
+									<strong>RESPECT TIME.</strong>
+								</div>
+								<hr />
+								Every Driver has a 30 min Grace period
+								to bring back the vehicle,
+								after that you will be charged a <span className="warning">late</span> fee.
+							</p>
+						</div>
+						<div className="card5">
+							<h3>Rule #5</h3>
+							<p className="rule-text">
+								<div className="rule-heading">
+									<strong className="rule-heading">Driver Eligibility Requirement: 
+								Valid Driver's License and Proof of
+								Active Insurance Coverage REQUIRED!</strong>
+								</div>
+								<hr />
+								At Sin's Exotic Car Rental, your safety and compliance come first. 
+								You must present a <strong><i><span className="warning">valid</span></i></strong> driver's license and proof of <strong><i><span className="warning">active</span></i></strong> insurance before taking the wheel of any vehicle in our
+								fleet. This policy ensures a smooth and secure rental experience for
+								everyone.
+							</p>
+						</div>
+					</div>
+				</div>
 
 				<div className="cars">
-					<h1>Our Most Popular</h1>
+					<h1>Our Most Popular Options</h1>
 					<div className="pics">
+						{/* These are going to be links */}
 					<img src="/src/assets/gwag1.png" alt="" className="car-pics" />
 					<img src="/src/assets/25m11.png" alt="" className="car-pics" />
 					<img src="/src/assets/urusperformante1.png" alt="" className="car-pics" />
